588f3a6268924c45e009cfdf446104b15e20d069
mrwinter09 <ivan.winter09@gmail.com>
2024-12-30 17:33:27 +0100

    client id

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,6 +1,6 @@
 import { createThirdwebClient } from "thirdweb";
 
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+const clientId = "5430ddfe188ae1d7fdb3902959158063";
 
 if (!clientId) {
   throw new Error("No client ID provided");
